dd7224c453c1711f85abacee8b3378068e42e777
Lucas <lucas.emanuel.carvalho@outlook.com>
2025-03-05 19:01:42 -0300

    Exercise 1 UPDATE

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,12 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
+       // Creates a box to insert the data 
        Console.Write("What is your first name? ");
+       // Use the data inserted and create a type attached to it
        string firstName = Console.ReadLine();
 
        Console.Write("What is your last name? ");
        string lastName = Console.ReadLine();
 
-       Console.WriteLine($"My name is {lastName}, {firstName} {lastName}");
+       // Remember to use the "$" to use the {} in the console. 
+       Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}");
     }
 }
